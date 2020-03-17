9c2951ef17326a7c11838eeded951a261786ef3f
srub <shani.rubovitch@gmail.com>
2020-03-17 16:53:27 +0100

    blink blue green with delay

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # main.py -- put your code here!
 import pyb
-led = pyb.LED(2)
-while true:
-    led.toggle()
+
+green = pyb.LED(2)
+blue = pyb.LED(4)
+
+while True:
+    green.toggle()
     pyb.delay(1000)
+    blue.toggle()
+    pyb.delay(500)
+
+    

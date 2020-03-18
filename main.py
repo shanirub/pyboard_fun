0d6b8d32636fe491b199c370f322df922c0f9d4f
srub <shani.rubovitch@gmail.com>
2020-03-18 09:27:35 +0100

    disco leds with blue changing intensity

--- main.py
+++ main.py
@@ -2,14 +2,21 @@
 import pyb
 
 leds = [pyb.LED(i) for i in range(1,5)]
+led = pyb.LED(4)
 
+for l in leds:
+    l.off()
+
+intensity = 0
 n = 0
 
 while True:
     n = (n + 1) % 4
     leds[n].toggle()
-    pyb.delay(50)
+    intensity = (intensity + 1) % 255
+    led.intensity(intensity)
+    pyb.delay(20)
 
-    
+
 
 

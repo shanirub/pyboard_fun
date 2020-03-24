741f0f55f4c8b998e26ae28f8d76f23667fa5a83
srub <shani.rubovitch@gmail.com>
2020-03-24 14:17:51 +0100

    accelerometer x testing

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 # main.py -- put your code here!
 import pyb
 
-leds = [pyb.LED(i) for i in range(1,5)]
-led = pyb.LED(4)
+accel = pyb.Accel()
+light = pyb.LED(3)
 
-for l in leds:
-    l.off()
-
-intensity = 0
-n = 0
+SENSITIVITY = 3
 
 while True:
-    n = (n + 1) % 4
-    leds[n].toggle()
-    intensity = (intensity + 1) % 255
-    led.intensity(intensity)
-    pyb.delay(20)
+    x = accel.x()
+    if abs(x) > SENSITIVITY:
+        light.on()
+    else:
+        light.off()
 
-
-
+    pyb.delay(100)
 

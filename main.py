ce1304a14944a869bbc6dfa8dcbc801e5df3ac84
srub <shani.rubovitch@gmail.com>
2020-03-24 17:11:47 +0100

    accelerometer, x+y+LEDs

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 # main.py -- put your code here!
 import pyb
 
-accel = pyb.Accel()
-light = pyb.LED(4)
-light.on()
-intensity = 0
+xlights = (pyb.LED(2), pyb.LED(3))
+ylights = (pyb.LED(1), pyb.LED(4))
 
+accel = pyb.Accel()
 SENSITIVITY = 3
 
 while True:
     x = accel.x()
-    if abs(x) > SENSITIVITY:
-        intensity = round(x * 8.5) % 255
-        light.intensity(intensity)
+    if x > SENSITIVITY:
+        xlights[0].on()
+        xlights[1].off()
+    elif x < -SENSITIVITY:
+        xlights[1].on()
+        xlights[0].off()
+    else:
+        xlights[0].off()
+        xlights[1].off()
 
-    pyb.delay(100)
+    y = accel.y()
+    if y > SENSITIVITY:
+        ylights[0].on()
+        ylights[1].off()
+    elif y < -SENSITIVITY:
+        ylights[1].on()
+        ylights[0].off()
+    else:
+        ylights[0].off()
+        ylights[1].off()
 
+    pyb.delay(200)
